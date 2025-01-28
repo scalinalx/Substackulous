b2843b3add4ff7387e749eb88536e6170fed441f
Stefan Acalin <stefan.acalin@gmail.com>
2025-01-28 16:58:40 +0200

    Fix Supabase auth configuration

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -13,8 +13,7 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: true,
-    flowType: 'pkce',
-    redirectTo: siteUrl
+    flowType: 'pkce'
   }
 });
 
